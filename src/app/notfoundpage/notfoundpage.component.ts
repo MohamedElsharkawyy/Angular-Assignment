9f70cd0f703d9506c1d7dd3af5ee1ea3531329b7
Marina10 <Marina10@marina.local>
2024-02-05 15:28:16 +0200

    first

---
+++ src/app/notfoundpage/notfoundpage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notfoundpage',
+  templateUrl: './notfoundpage.component.html',
+  styleUrls: ['./notfoundpage.component.css']
+})
+export class NotfoundpageComponent {
+
+}
